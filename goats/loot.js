b2f4a3c426ee5e6b13d82cc83e557dc4f56210ff
Gabe R <rodriguezgab1601@gmail.com>
2019-02-22 11:06:12 +0800

    Added loot and reminder commands

---
+++ goats/loot.js
@@ -0,0 +1,24 @@
+const embed = {
+  color: 32896,
+  thumbnail: {
+    url: "http://cdn.aurakingdom-db.com/images/icons/I00025.jpg"
+  },
+  footer: {
+    text: "Opening the chest gives a chance in obtaining any one of these items"
+  },
+  fields: [
+    {
+      name: "Loot | BattleFront Merit Chest",
+      value:
+        "• Hidden Demon's Hood\n• Baphomet's Assault Armor\n• Demon's Fortified Warhelm\n• Bisolen's Bulwark Armor\n• Healing Potion (Non-Tradable)\n• Secret Stone Randomizer (Non-Tradable)\n• Treasure Charm (Non-Tradable)\n• 50 Loyalty Points (Non-Tradable)\n• Aura Kingdom Coupon: 2000 Points\n• Banoleth Scythe\n• King Banoleth Scythe"
+    }
+  ]
+};
+
+module.exports = {
+  name: "loot",
+  description: "loot list",
+  execute(message, args) {
+    message.channel.send({ embed });
+  }
+};
---
+++ goats/remind.js
@@ -0,0 +1,29 @@
+const embed = {
+  description:
+    "Reminders |  Activating this function will make the bot automatically send relevant commands after reaching set countdowns",
+  color: 32896,
+  footer: {
+    text:
+      "Messages are sent when countdowns are at: 60 minutes and 10 minutes before upcoming spawns. \nTo clear all reminders, type +remindstop"
+  },
+  fields: [
+    {
+      name: "Personal",
+      value:
+        "• Bot will make reminders through direct messages\n• To activate, type `+remindme` followed by a command\n• To deactivate, type `+remindme!` followed by the command\n• Example: `+remindme goatsc` | `+remindme! goatsc`"
+    },
+    {
+      name: "Channel",
+      value:
+        "• Bot will make reminders through the channel it was activated in\n• To activate, type `+remindhere` followed by a command\n• To deactivate, type `+remindhere!` followed by the command\n• Example: `+remindhere goatsp` | `+remindhere! goatsp`"
+    }
+  ]
+};
+
+module.exports = {
+  name: "remind",
+  description: "reminder help",
+  execute(message, args) {
+    message.channel.send({ embed });
+  }
+};
