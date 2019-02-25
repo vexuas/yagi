9fb03a1779c592009bfa73c2900f1a764ad5c232
Gabe R <rodriguezgab1601@gmail.com>
2019-02-25 11:29:06 +0800

    Updated remind info and Added user remind list codes

---
+++ goats/remindmelist.js
@@ -0,0 +1,40 @@
+const { chimuserlist } = require("./chimusers.js");
+const { phoeuserlist } = require("./phoeusers.js");
+
+const reminders = {
+  goatsc: "• Chimera Goat Times (`+goatsc`)",
+  goatsp: "• Phoenix Goat Times (`+goatsp`)"
+};
+
+let userReminders = [];
+function getListReminders(message) {
+  userReminders = [];
+  if (chimuserlist[message.author.username] === "chimEnable") {
+    userReminders.push("goatsc");
+  }
+  if (phoeuserlist[message.author.username] === "phoeEnable") {
+    userReminders.push("goatsp");
+  }
+  const reminderOutput = userReminders
+    .map(item => {
+      return reminders[item];
+    })
+    .join("\n");
+  const embed = {
+    title: `${message.author.tag} | Active User Reminders`,
+    description: `${reminderOutput}`,
+    color: 32896
+  };
+  if (reminderOutput === "") {
+    return message.channel.send("You have no active reminders");
+  }
+  message.channel.send({ embed });
+}
+
+module.exports = {
+  name: "remindme list",
+  description: "lists active reminders",
+  execute(message, args) {
+    getListReminders(message);
+  }
+};
