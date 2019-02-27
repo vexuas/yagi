b3ad8e62e5b12bb0fa966fe56a2f9c3d235db693
Gabe R <rodriguezgab1601@gmail.com>
2019-02-27 14:13:51 +0800

    Reminder functions and user/channel list refresh

--- yagi.js
+++ yagi.js
@@ -3,15 +3,29 @@ const Discord = require("discord.js");
 const { prefix, token } = require("./config.json");
 const { chimidlist, chimuserlist } = require("./goats/chimusers");
 const { phoeidlist, phoeuserlist } = require("./goats/phoeusers");
-const { chimeraTimer, chimIntervals } = require("./goats/ctimer");
-const { phoenixTimer, phoeIntervals } = require("./goats/ptimer");
+const { chimchannelid, chimchannellist } = require("./goats/chimchannels");
+const { phoechannelid, phoechannellist } = require("./goats/phoechannels");
+const {
+  chimeraTimer,
+  chimUserIntervals,
+  chimChanIntervals
+} = require("./goats/ctimer");
+const {
+  phoenixTimer,
+  phoeUserIntervals,
+  phoeChanIntervals
+} = require("./goats/ptimer");
 
 let chimIds = Object.values(chimidlist);
 let phoeIds = Object.values(phoeidlist);
+let chimchannelIds = Object.values(chimchannelid);
+let phoechannelIds = Object.values(phoechannelid);
 const yagi = new Discord.Client();
 yagi.commands = new Discord.Collection();
 console.log(chimIds);
+console.log(Object.keys(chimuserlist));
 console.log(phoeIds);
+console.log(Object.keys(phoeuserlist));
 const commandFiles = fs
   .readdirSync("./goats")
   .filter(file => file.endsWith(".js"));
@@ -31,42 +45,116 @@ const activitylist = [
   "+goatsp for Phoenix wb",
   "Last update: 25/02/2019"
 ];
-//Add and removes user reminders
+/**
+ * Add and removes user reminders
+ * Every add/remove refreshes all reminders
+ */
 function addChimUser(username, userid) {
   chimidlist[username] = userid;
   chimIds = Object.values(chimidlist);
   console.log(chimIds);
-  chimReminderSend(chimIds);
+  refreshReminders();
 }
 function removeChimUser(username) {
   delete chimidlist[username];
   chimIds = Object.values(chimidlist);
   console.log(chimIds);
-  chimReminderSend(chimIds);
+  refreshReminders();
 }
 function addPhoeUser(username, userid) {
   phoeidlist[username] = userid;
   phoeIds = Object.values(phoeidlist);
-  phoeReminderSend(phoeIds);
+  console.log(phoeIds);
+  refreshReminders();
 }
 function removePhoeUser(username) {
   delete phoeidlist[username];
   phoeIds = Object.values(phoeidlist);
-  phoeReminderSend(phoeIds);
+  console.log(phoeIds);
+  refreshReminders();
 }
-//Starts reminder countdown for users
-function chimReminderSend(chimera) {
+//Add and removes channel reminders
+function addChimChannel(channelname, channelid) {
+  chimchannelid[channelname] = channelid;
+  chimchannelIds = Object.values(chimchannelid);
+  console.log(chimchannelIds);
+  refreshReminders();
+}
+function removeChimChannel(channelname) {
+  delete chimchannelid[channelname];
+  chimchannelIds = Object.values(chimchannelid);
+  console.log(chimchannelIds);
+  refreshReminders();
+}
+function addPhoeChannel(channelname, channelid) {
+  phoechannelid[channelname] = channelid;
+  phoechannelIds = Object.values(phoechannelid);
+  console.log(phoechannelIds);
+  refreshReminders();
+}
+function removePhoeChannel(channelname) {
+  delete phoechannelid[channelname];
+  phoechannelIds = Object.values(phoechannelid);
+  console.log(phoechannelIds);
+  refreshReminders();
+}
+//Starts reminder countdowns
+function chimUserReminderSend(chimera) {
   chimera.forEach(item => {
     const chimuserId = yagi.users.get(item);
-    chimeraTimer(chimuserId);
+    chimeraTimer(chimuserId, chimUserIntervals);
   });
 }
-function phoeReminderSend(phoenix) {
+function phoeUserReminderSend(phoenix) {
   phoenix.forEach(item => {
     const phoeuserId = yagi.users.get(item);
-    phoenixTimer(phoeuserId);
+    phoenixTimer(phoeuserId, phoeUserIntervals);
   });
 }
+
+function chimChannelReminderSend(chimera) {
+  chimera.forEach(item => {
+    const chimuserId = yagi.channels.get(item);
+    chimeraTimer(chimuserId, chimChanIntervals);
+  });
+}
+function phoeChannelReminderSend(phoenix) {
+  phoenix.forEach(item => {
+    const phoeuserId = yagi.channels.get(item);
+    phoenixTimer(phoeuserId, phoeChanIntervals);
+  });
+}
+//Refreshing reminder intervals
+function sendReminders() {
+  chimUserReminderSend(chimIds);
+  phoeUserReminderSend(phoeIds);
+  chimChannelReminderSend(chimchannelIds);
+  phoeChannelReminderSend(phoechannelIds);
+}
+
+function clearReminders() {
+  chimUserIntervals.forEach(interval => {
+    clearInterval(interval);
+  });
+  chimUserIntervals.splice(0, chimUserIntervals.length);
+  phoeUserIntervals.forEach(interval => {
+    clearInterval(interval);
+  });
+  phoeUserIntervals.splice(0, phoeUserIntervals.length);
+  chimChanIntervals.forEach(interval => {
+    clearInterval(interval);
+  });
+  chimChanIntervals.splice(0, chimChanIntervals.length);
+  phoeChanIntervals.forEach(interval => {
+    clearInterval(interval);
+  });
+  phoeChanIntervals.splice(0, phoeChanIntervals.length);
+}
+function refreshReminders() {
+  clearReminders();
+  sendReminders();
+}
+
 yagi.on("ready", () => {
   yagi.user.setActivity(activitylist[0]);
   let channel = yagi.channels.get("491832593529045003");
@@ -75,26 +163,16 @@ yagi.on("ready", () => {
     console.log(item.username);
     console.log(item.id);
   });
-  chimReminderSend(chimIds);
-  phoeReminderSend(phoeIds);
+  sendReminders();
   setInterval(() => {
     const index = Math.floor(Math.random() * (activitylist.length - 1) + 1);
     yagi.user.setActivity(activitylist[index]);
   }, 120000);
   //Gets data from sheet every 10 minutes
   setInterval(() => {
-    chimIntervals.forEach(interval => {
-      clearInterval(interval);
-    });
-    chimIntervals.splice(0, chimIntervals.length);
-    chimReminderSend(chimIds);
-    phoeIntervals.forEach(interval => {
-      clearInterval(interval);
-    });
-    phoeIntervals.splice(0, phoeIntervals.length);
-    phoeReminderSend(phoeIds);
+    refreshReminders();
     console.log("10 minute sheet checkup");
-  }, 600000);
+  }, 10000);
 });
 
 yagi.login(token);
@@ -105,45 +183,6 @@ yagi.on("message", message => {
   const args = message.content.slice(prefix.length).split();
   const command = args.shift().toLowerCase();
   //if command doesn't exist, show error message
-  let userid = message.author.id;
-  let username = message.author.username;
-  if (message.content === "+remindme goatsc") {
-    if (chimuserlist[username] !== "chimEnable") {
-      chimIntervals.forEach(interval => {
-        clearInterval(interval);
-      });
-      chimIntervals.splice(0, chimIntervals.length);
-      addChimUser(username, userid);
-    }
-  }
-  if (message.content === "+remindme! goatsc") {
-    if (chimuserlist[username] !== "chimDisable") {
-      chimIntervals.forEach(interval => {
-        clearInterval(interval);
-      });
-      chimIntervals.splice(0, chimIntervals.length);
-      removeChimUser(username);
-    }
-  }
-  if (message.content === "+remindme goatsp") {
-    if (phoeuserlist[username] !== "phoeEnable") {
-      phoeIntervals.forEach(interval => {
-        clearInterval(interval);
-      });
-      phoeIntervals.splice(0, phoeIntervals.length);
-      addPhoeUser(username, userid);
-    }
-  }
-  if (message.content === "+remindme! goatsp") {
-    if (phoeuserlist[username] !== "phoeDisable") {
-      phoeIntervals.forEach(interval => {
-        clearInterval(interval);
-      });
-    }
-    phoeIntervals.splice(0, phoeIntervals.length);
-    removePhoeUser(username);
-  }
-
   if (!yagi.commands.has(command))
     return message.channel.send("type `+info` for commands list");
   //if it does, do command
@@ -157,3 +196,12 @@ yagi.on("message", message => {
     console.error(error);
   }
 });
+
+module.exports.addChimUser = addChimUser;
+module.exports.addChimChannel = addChimChannel;
+module.exports.addPhoeUser = addPhoeUser;
+module.exports.addPhoeChannel = addPhoeChannel;
+module.exports.removeChimUser = removeChimUser;
+module.exports.removeChimChannel = removeChimChannel;
+module.exports.removePhoeUser = removePhoeUser;
+module.exports.removePhoeChannel = removePhoeChannel;
