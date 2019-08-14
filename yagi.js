be22f203d23fb0794fe4070140fc35bc336ae825
Gabe R <rodriguezgab1601@gmail.com>
2019-08-14 22:57:05 +0800

    Add todo

--- yagi.js
+++ yagi.js
@@ -4,6 +4,22 @@ const commands = require('./commands');
 
 const yagi = new Discord.Client();
 
+/**
+ * TODO | By 19 August 2019 | 7 days
+ * ---------------------------------
+ * Add cooldown for commands to avoid spamming bot
+ * Add ability to customize prefix
+ * Remove legacy code
+ * Rethink the amount of use for mentions
+ * Refactor info command to be an actual information hub
+ * Add help command for command list
+ * Make help command as a dm, info too?
+ * Create a database
+ * Rework how reminders work
+ * Set up API
+ * Have a drink with nami
+ */
+
 yagi.once('ready', () => {
   console.log("I'm ready! (◕ᴗ◕✿)");
   /**
