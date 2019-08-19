eb53ca6a9a49d8c9b1d698bd14385fc51664044b
Gabe R <rodriguezgab1601@gmail.com>
2019-08-20 00:23:01 +0800

    Boilerplate for goat file testing

---
+++ test/goats.js
@@ -0,0 +1,8 @@
+const assert = require('assert');
+const { getCountdown } = require('./commands/timer/goats');
+
+describe('goats', function() {
+  describe('getCountdown', function() {
+    //boilerplate for goat testing
+  });
+});
