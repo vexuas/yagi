4a9d2d218eef8ed43d9f8f1d979423213e44de15
Gabe R <rodriguezgab1601@gmail.com>
2019-09-23 02:02:57 +0800

    Change look of sheets embed

--- commands/hub/sheets.js
+++ commands/hub/sheets.js
@@ -1,21 +1,19 @@
-const embed = {
-  color: 32896,
-  footer: {
-    text: 'Kudos to all the leads and editors for being awesome and making all this possible!'
-  },
-  fields: [
-    {
-      name: 'Phoenix',
-      value:
-        '• [Lazy Goat](https://tinyurl.com/LazyGoatWB)\n• [Angels](https://docs.google.com/spreadsheets/d/1MrMwNerILxNK0lvKCBklkYZx_OKAb4io8XdaldRyO_g/edit#gid=1305398803)',
-      inline: true
-    },
-    {
-      name: 'Chimera',
-      value: '• [Catalyst](https://tinyurl.com/catalyst-ak)',
-      inline: true
-    }
-  ]
+const generateEmbed = function generateEmbedForGoatSheets(message) {
+  const embed = {
+    title: 'Public Sheets',
+    description:
+      'Kudos to all the leads and editors for being awesome and making all this possible!\nFor more information about the Olympus World Boss, do visit these links!\n[Olympus Sheet](https://docs.google.com/spreadsheets/d/tUL0-Nn3Jx7e6uX3k4_yifQ/edit#gid=585652389) | [Olympus Discord](https://discordapp.com/invite/bC9zWYs) | [Olympus Recruitment](https://docs.google.com/forms/d/e/1FAIpQLSfcLEBugvL70lLCttUiSHw_Cc6wI8S1te6X-CAsP8N-dr6u_w/viewform)',
+    color: 32896,
+    fields: [
+      {
+        name: 'OG links',
+        value:
+          '[Catalyst (Chimera)](https://tinyurl.com/catalyst-ak)\n[Angels (Phoenix)](https://docs.google.com/spreadsheets/d/1MrMwNerILxNK0lvKCBklkYZx_OKAb4io8XdaldRyO_g/edit#gid=1305398803)\n[Lazy Goat (Phoenix)](https://tinyurl.com/LazyGoatWB)',
+        inline: true
+      }
+    ]
+  };
+  return embed;
 };
 
 module.exports = {
@@ -23,7 +21,11 @@ module.exports = {
   description:
     'A collection of current and previous sheets used by leads/editors to keep track of World Boss',
   execute(message) {
-    embed.description = `${message.author} | Public Sheets`;
-    message.channel.send({ embed });
+    try {
+      const embed = generateEmbed(message);
+      message.channel.send({ embed });
+    } catch (e) {
+      console.log(e);
+    }
   }
 };
