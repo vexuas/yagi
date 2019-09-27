4cf0681861e52595a1ed78cdbb5795f6818db4a7
Gabe R <rodriguezgab1601@gmail.com>
2019-09-28 04:53:06 +0800

    Fix current prefix not updating in info embed

--- commands/hub/info.js
+++ commands/hub/info.js
@@ -2,14 +2,14 @@ const { format } = require('date-fns');
 const package = require('../../package.json');
 const grvAcnt = '`';
 
-const sendInfoEmbed = function designOfYagiInformationEmbed(message, yagi) {
+const sendInfoEmbed = function designOfYagiInformationEmbed(message, yagi, yagiPrefix) {
   const embed = {
     title: 'Info',
     description: `Hi there! I'm Yagi and I provide information for the world bosses of Vulture's Vale and Blizzard Berg in Aura Kingdom EN!\n\nInitially my creator only wanted to make a [website](https://ak-goats.com/) but eventually opted to make a discord version as well! Since then I'm currently serving ${
       yagi.users.size
     } people in ${
       yagi.guilds.size
-    } servers!\n\nMy timer data is extracted from the player-run [Olympus WB Sheet](https://docs.google.com/spreadsheets/d/tUL0-Nn3Jx7e6uX3k4_yifQ/edit#gid=585652389). Kudos to the hardwork of the editors and leads in the team!\n\nMy prefix  is ${grvAcnt}biso!${grvAcnt} | For a detailed list of my commands, type ${grvAcnt}biso! help${grvAcnt}`,
+    } servers!\n\nMy timer data is extracted from the player-run [Olympus WB Sheet](https://docs.google.com/spreadsheets/d/tUL0-Nn3Jx7e6uX3k4_yifQ/edit#gid=585652389). Kudos to the hardwork of the editors and leads in the team!\n\nMy prefix  is ${grvAcnt}${yagiPrefix}${grvAcnt} | For a detailed list of my commands, type ${grvAcnt}${yagiPrefix}help${grvAcnt}`,
     color: 32896,
     thumbnail: {
       url:
@@ -44,7 +44,7 @@ const sendInfoEmbed = function designOfYagiInformationEmbed(message, yagi) {
 module.exports = {
   name: 'info',
   description: 'The story and information hub of yagi',
-  execute(message, arguments, yagi) {
-    sendInfoEmbed(message, yagi);
+  execute(message, arguments, yagi, commands, yagiPrefix) {
+    sendInfoEmbed(message, yagi, yagiPrefix);
   }
 };
