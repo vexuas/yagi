536076f5cb1eedb0d22b8a8ca017d1dea55cd34e
Gabe R <rodriguezgab1601@gmail.com>
2019-09-28 04:10:25 +0800

    Almost forgot the release logs

--- commands/hub/release.js
+++ commands/hub/release.js
@@ -2,11 +2,12 @@ module.exports = {
   name: 'release',
   description: 'Displays the latest version information and changelog',
   execute(message) {
-    const release = '```• Minor fix to phoenix timer    \n• Update sheets list```';
+    const release =
+      "```v2 has been released! Such a big change so can't write it here :c Do check out the offical release notes though!```";
     const embed = {
       description: `${
         message.author
-      } | Latest Release: [v1.4](https://github.com/Vexuas/yagi/releases)\n${release}`,
+      } | Latest Release: [v2.0.0](https://github.com/Vexuas/yagi/releases)\n${release}`,
       color: 32896
     };
     message.channel.send({ embed });
