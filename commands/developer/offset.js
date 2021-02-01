8d139f2109be4ba9f5c9b4416316cf5be8b3d894
Gabe R <rodriguezgab1601@gmail.com>
2021-02-01 17:42:20 +0800

    Add functions to toggle offset

--- commands/developer/offset.js
+++ commands/developer/offset.js
@@ -1,23 +1,54 @@
 const { currentOffset, dstOffset, normalOffset, start, end } = require('../../config/offset.json');
+const fs = require('fs');
 
-const embed = {
-  title: 'Offset Changed!',
-  color: 32896,
-  fields: [
-    {
-      name: 'Current Offset',
-      value: currentOffset
+const generateEmbed = (offset) => {
+  const embed = {
+    title: 'Offset Changed!',
+    color: 32896,
+    fields: [
+      {
+        name: 'Current Offset',
+        value: offset
+      }
+    ]
+  };
+  return embed;
+}
+
+
+const toggleOffset = (message) => {
+  let offset;
+
+  if(currentOffset === 4){
+    offset = 5;
+  } else if(currentOffset === 5){
+    offset = 4;
+  }
+
+  const updatedOffsetInfo = {
+    currentOffset: offset,
+    start,
+    end,
+    dstOffset,
+    normalOffset
+  };
+  const embed = generateEmbed(offset);
+
+  fs.writeFile('./config/offset.json', JSON.stringify(updatedOffsetInfo, null, 2), function (err) {
+    if (err) {
+      return console.log(err);
     }
-  ]
-};
-
+    return message.channel.send({ embed })
+  });
+}
+  
 module.exports = {
   name: 'offset',
   description: "Switches offset between normal and dst",
   devOnly: true,
   async execute(message) {
     if (message.author.id === '183444648360935424') {
-      return message.channel.send({ embed });
+      return toggleOffset(message);
     }
     return;
   }
