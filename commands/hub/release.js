ce12b5fdb7c193d32b0c03b60309fcce56aebaed
Gabe R <rodriguezgab1601@gmail.com>
2021-02-01 18:30:32 +0800

    Update release info

--- commands/hub/release.js
+++ commands/hub/release.js
@@ -4,7 +4,7 @@ module.exports = {
   execute(message) {
     const release = '```Tbh just a lot of internal updates. Gonna see if I can make this release command more efficient in the coming weeks```';
     const embed = {
-      description: `${message.author} | Latest Release: [v2.2.0](https://github.com/Vexuas/yagi/releases)\n${release}`,
+      description: `${message.author} | Latest Release: [v2.3.0](https://github.com/Vexuas/yagi/releases)\n${release}`,
       color: 32896,
     };
     message.channel.send({ embed });
