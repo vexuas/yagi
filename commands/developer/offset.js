15e7f575229ee760d178797ca0b9abda34541710
Gabe R <rodriguezgab1601@gmail.com>
2021-02-01 17:49:11 +0800

    Add comment

--- commands/developer/offset.js
+++ commands/developer/offset.js
@@ -15,7 +15,13 @@ const generateEmbed = (offset) => {
   return embed;
 }
 
-
+/**
+ * Simple way to toggle between offsets
+ * Checks if currentOffset is one value and sets the new offset to the other
+ * Setting the value here isn't enough, we have to update the json file where the timers gets the offset from
+ * Since I initially used filesync to write and update files when I started, used it again here for consistency
+ * This works nicely but note in the future to research better handle data states
+ */
 const toggleOffset = (message) => {
   let offset;
 
