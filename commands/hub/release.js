7b1b4d4e610e4deb995c410a545c0e1b0a56a0b3
Gabriel R <42207245+vexuas@users.noreply.github.com>
2021-02-16 05:54:26 +0800

    Release/2020 02 16 (#46)
    * Update CHANGELOG.md [skip ci]
    
    * v2.3.1
    
    * Update release date
    
    * Update release notes

--- commands/hub/release.js
+++ commands/hub/release.js
@@ -2,9 +2,9 @@ module.exports = {
   name: 'release',
   description: 'Displays the latest version information and changelog',
   execute(message) {
-    const release = '```Tbh just a lot of internal updates. Gonna see if I can make this release command more efficient in the coming weeks```';
+    const release = '```Add temporary warning when using the prefix command```';
     const embed = {
-      description: `${message.author} | Latest Release: [v2.3.0](https://github.com/Vexuas/yagi/releases)\n${release}`,
+      description: `${message.author} | Latest Release: [v2.3.1](https://github.com/Vexuas/yagi/releases)\n${release}`,
       color: 32896,
     };
     message.channel.send({ embed });
