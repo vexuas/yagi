a0af35d30df5ff34d60ff46f49436b3499acd5c2
Gabriel R <42207245+vexuas@users.noreply.github.com>
2021-03-10 02:04:42 +0800

    Release/10 03 2021 (#49)
    * Update CHANGELOG.md [skip ci]
    
    * v2.4.0
    
    * Update date
    
    * Update release notes

--- commands/hub/release.js
+++ commands/hub/release.js
@@ -2,9 +2,9 @@ module.exports = {
   name: 'release',
   description: 'Displays the latest version information and changelog',
   execute(message) {
-    const release = '```Add temporary warning when using the prefix command```';
+    const release = '```Add PSA dev command```';
     const embed = {
-      description: `${message.author} | Latest Release: [v2.3.1](https://github.com/Vexuas/yagi/releases)\n${release}`,
+      description: `${message.author} | Latest Release: [v2.4.0](https://github.com/Vexuas/yagi/releases)\n${release}`,
       color: 32896,
     };
     message.channel.send({ embed });
