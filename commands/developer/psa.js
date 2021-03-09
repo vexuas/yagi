33b2577e1874e19fef4cf91bd3fc50694bb6fa18
Gabriel R <42207245+vexuas@users.noreply.github.com>
2021-03-10 01:10:06 +0800

    Add PSA command (#47)
    * Create psa command
    
    * Add psa json
    
    * Add condition to show psa when using the timer
    
    * Add show function
    
    * Add stop psa function
    
    * Add embed function
    
    * Fix error
    
    * Add comments
    
    * Add start function
    
    * Add switch conditions to parent function
    
    * Delete psa.json
    
    * Remove logs
    
    * Update default psa message

---
+++ commands/developer/psa.js
@@ -0,0 +1,119 @@
+const { PSA, PSAmessage } = require('../../config/psa.json');
+const fs = require('fs');
+
+const defaultMessage = "There looks to be a problem with the timer at the moment, sorry! （・□・；)"
+
+/**
+ * Shows if PSA is activated
+ */
+const showPSA = (message) => {
+  const embed = generateEmbed('show');
+  return message.channel.send({ embed });
+}
+//-----
+/**
+ * Turns on the PSA
+ * This would send the PSA message when users try to use the goat timer
+ * @param {string} customPSA - optional message if you want to change the message of the PSA
+ */
+const startPSA = (message, customPSA) => {
+  if(PSA){
+    const embed = generateEmbed('start', 'PSA is already turned on!');
+    return message.channel.send({ embed });
+  }
+  const updatedPSAInfo = {
+    PSA: true,
+    PSAmessage: customPSA ? customPSA : defaultMessage
+  }
+
+  fs.writeFile('./config/psa.json', JSON.stringify(updatedPSAInfo, null, 2), function (err) {
+    if (err) {
+      return console.log(err);
+    }
+    const embed = generateEmbed('start', null, updatedPSAInfo);
+    message.channel.send({ embed });
+  });
+}
+//-----
+/**
+ * Turns off PSA
+ * Also switches the message back to the default one
+ */
+const stopPSA = (message) => {
+  if(!PSA){
+    const embed = generateEmbed('stop', 'PSA is already turned off!');
+    return message.channel.send({ embed });
+  }
+  const updatedPSAInfo = {
+    PSA: false,
+    PSAmessage: defaultMessage
+  }
+
+  fs.writeFile('./config/psa.json', JSON.stringify(updatedPSAInfo, null, 2), function (err) {
+    if (err) {
+      return console.log(err);
+    }
+    const embed = generateEmbed('stop', null, updatedPSAInfo);
+    message.channel.send({ embed });
+  });
+}
+//-----
+/**
+ * Reusable embed to use for this command
+ * @param {string} type - whether argument is show, start, stop
+ * @param {string} descriptionInfo - if we want to add a custom description
+ * @param {object} updatedInfo - additional updated info to display
+ */
+const generateEmbed = (type, descriptionInfo, updatedInfo) => {
+  let description;
+  const PSAtext = PSA ? 'On' : 'Off';
+  const updatedPSAtext = updatedInfo && updatedInfo.PSA ? 'On' : 'Off';
+
+  switch(type){
+    case 'show':
+      description = PSA ? 'PSA is turned on' : 'PSA is turned off';
+      break;
+    case 'stop':
+      description = descriptionInfo ? descriptionInfo : 'Turned off PSA!';
+      break;
+    case 'start':
+      description = descriptionInfo ? descriptionInfo : 'Turned on PSA!';
+      break;
+  }
+  const embed = {
+    description,
+    color: 32896,
+    fields: [
+      {
+        name: 'PSA',
+        value: updatedInfo ? updatedPSAtext : PSAtext
+      },
+      {
+        name: 'PSA Message',
+        value: updatedInfo && updatedInfo.PSAmessage ? updatedInfo.PSAmessage : PSAmessage
+      }
+    ]
+  }
+  return embed;
+}
+module.exports = {
+  name: "psa",
+  description: "Toggles a trigger that sends a public service announcement when using the timer. Only to be used in extreme cases where timer data is experiencing problems",
+  hasArguments: true,
+  devOnly: true,
+  async execute(message, arguments) {
+    if (message.author.id === '183444648360935424') {
+      const type = arguments.split(' ', 1).toString();
+      switch(type){
+        case 'show':
+          return showPSA(message);
+        case 'start':
+          const customMessage = arguments.slice(type.length + 1);
+          return startPSA(message, customMessage);
+        case 'stop':
+          return stopPSA(message);
+      }
+    }
+    return;
+  }
+};
