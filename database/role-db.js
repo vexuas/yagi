87b8a1ba50974847e42b58a864bc99ab06b2f9b7
Gabe R <rodriguezgab1601@gmail.com>
2021-07-07 08:49:19 +0800

    Add comments

--- database/role-db.js
+++ database/role-db.js
@@ -1,10 +1,23 @@
 const sqlite = require('sqlite3').verbose();
 const { generateUUID } = require('../helpers');
-
+/**
+ * Creates Role table inside the Yagi Database
+ * Gets called in the client.once("ready") hook
+ * @param database - yagi database
+ * @param guilds - guilds that yagi is in; using this as roles are nested inside the guild collection
+ * @param client - yagi client
+ */
 const createRoleTable = async (database, guilds, client) => {
+  //Wrapped in a serialize to ensure that each method is called in order which its initialised
   database.serialize(() => {
+    //Creates Role Table with the relevant columns if it does not exist
     database.run('CREATE TABLE IF NOT EXISTS Role(uuid TEXT NOT NULL PRIMARY KEY, role_id TEXT NOT NULL, name TEXT NOT NULL, created_at TEXT NOT NULL, member_count INTEGER NOT NULL, color TEXT NOT NULL, guild_id TEXT NOT NULL, used_for_reminder BOOLEAN, reminder_id TEXT)');
     
+    /**
+     * Populate Role Table with existing roles for every guild
+     * As role_id is only unique to the guild that it is in, there is a possiblity that one role from one guild would have the same id as another role in another guild
+     * With this risk, we can't make role_id as the primary key and instead create a randomized uuid 
+     **/ 
     guilds.forEach(guild => {
       guild.roles.cache.forEach(async role => {
         await guild.members.fetch();
