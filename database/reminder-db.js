a7559f08a927612a367ba74055e957ab1a6ba110
Gabe R <rodriguezgab1601@gmail.com>
2021-07-13 07:39:28 +0800

    Only allow 1 reminder to be active in a server

--- database/reminder-db.js
+++ database/reminder-db.js
@@ -35,6 +35,12 @@ const insertNewReminder = (message) => {
       if(err){
         console.log(err);
       }
+      const embed = {
+        title: "Reminder Enabled!",
+        description: "I will notify you in this channel before world boss spawns!",
+        color: 55296
+      }
+      message.channel.send({ embed });
     })
     createReminderRole(message.guild, reminderUUID);
   })
@@ -48,44 +54,49 @@ const enableReminder = (message) => {
   let database = new sqlite.Database('./database/yagi.db', sqlite.OPEN_READWRITE);
   //Wrapped in a serialize to ensure that each method is called in order which its initialised
   database.serialize(() => {
-    database.get(`SELECT * FROM Reminder WHERE guild_id = ${message.guild.id} AND channel_id = ${message.channel.id}`, (error, reminder) => {
+    database.get(`SELECT * FROM Reminder WHERE guild_id = ${message.guild.id} AND enabled = ${true}`, (error, enabledReminder) => {
       if(error){
         console.log(error);
       }
-      const embed = enableReminderEmbed(message, reminder);
-      //Checks if it's an existing reminder
-      if(reminder){
-        //If it is, checks if the reminder is enabled
-        if(reminder.enabled === 1){
-          message.channel.send({ embed });
-        } else {
-          /**
-           * Additional call to the Role Table to check if the role associated with the reminder still exists and hasn't been deleted
-           * If it does exist, we don't do anything and update the reminder to its enable state
-           * If it has been deleted, we call the createReminderRole to create a new role and link it with the reminder before updating it to its enable state
-           */
-          database.serialize(() => {
-            database.get(`SELECT * FROM Role WHERE uuid = "${reminder.role_uuid}"`, (err, role) => {
-              if(err){
-                console.log(err);
-              }
-              if(!role){
-                createReminderRole(message.guild, reminder.uuid);
-              }
-            })
-            //Updates the reminder to enabled with relevant data
-            database.run(`UPDATE Reminder SET enabled = ${true}, enabled_by = "${message.author.id}", enabled_at = ${Date.now()} WHERE uuid = "${reminder.uuid}"`, err => {
-              if(err){
-                console.log(err);
-              }
-              message.channel.send({ embed });
-            })
-          })
-        }
+
+      if(enabledReminder){
+        const embed = enableReminderEmbed(message, enabledReminder)
+        message.channel.send({ embed })
       } else {
-        //Creates a new reminder if it doesn't exist
-        insertNewReminder(message);
-        message.channel.send({ embed });
+        database.get(`SELECT * FROM Reminder WHERE guild_id = ${message.guild.id} AND channel_id = ${message.channel.id}`, (error, reminder) => {
+          if(error){
+            console.log(error);
+          }
+          //Checks if it's an existing reminder
+          if(reminder){
+            /**
+             * Additional call to the Role Table to check if the role associated with the reminder still exists and hasn't been deleted
+             * If it does exist, we don't do anything and update the reminder to its enable state
+             * If it has been deleted, we call the createReminderRole to create a new role and link it with the reminder before updating it to its enable state
+             */
+            database.serialize(() => {
+              database.get(`SELECT * FROM Role WHERE uuid = "${reminder.role_uuid}"`, (err, role) => {
+                if(err){
+                  console.log(err);
+                }
+                if(!role){
+                  createReminderRole(message.guild, reminder.uuid);
+                }
+              })
+              //Updates the reminder to enabled with relevant data
+              database.run(`UPDATE Reminder SET enabled = ${true}, enabled_by = "${message.author.id}", enabled_at = ${Date.now()} WHERE uuid = "${reminder.uuid}"`, err => {
+                if(err){
+                  console.log(err);
+                }
+                const embed = enableReminderEmbed(message, reminder)
+                message.channel.send({ embed });
+              })
+            })
+          } else {
+            //Creates a new reminder if it doesn't exist
+            insertNewReminder(message);
+          }
+        })
       }
     })
   })
