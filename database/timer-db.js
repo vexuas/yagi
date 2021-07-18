aeccae9e557c1a58531b9ac08dd8b6a8750a976e
Gabe R <rodriguezgab1601@gmail.com>
2021-07-18 20:54:10 +0800

    Add comments

--- database/timer-db.js
+++ database/timer-db.js
@@ -1,21 +1,41 @@
 const sqlite = require('sqlite3').verbose();
 const { generateUUID, getWorldBossData, getServerTime, validateWorldBossData } = require('../helpers');
 
+/**
+ * Creates Timer table inside the Yagi Database
+ * Gets called in the client.once("ready") hook
+ * Primarily used to keep track of the world boss data without having to always request the data in the spreadsheet
+ * This is useful as the dynamic nature of the world boss timings would require having multiple calls in between intervals to know if the data is accurate
+ * In saving the timer data in our end, we lessen the number of those calls and the complexity it brings
+ * @param database - yagi database
+ * @param worldBoss - validated world boss object data; note that we want our database to be as accurate as possible hence we don't use the raw data from the sheet
+ */
 const createTimerTable = (database, worldBoss) => {
+  //Wrapped in a serlialize to ensure that each method is called in order in which it is initialised
   database.serialize(() => {
+    //Creates Timer table with relevant columns if it does not exist
     database.run('CREATE TABLE IF NOT EXISTS Timer(uuid TEXT NOT NULL, last_retrieved_at DATE NOT NULL, location TEXT NOT NULL, next_spawn TEXT NOT NULL, projected_next_spawn TEXT NOT NULL, accurate BOOLEAN NOT NULL)');
 
     database.get(`SELECT * FROM Timer WHERE rowid = ${1}`, (error, timer) => {
       if(error){
         console.log(error);
       }
-      if(!timer){
+      //Check if there's an existing timer in our database
+      if(timer){
+        //Update the timer if it does
+        updateTimerData(worldBoss, timer);
+      } else {
+        //Create a new timer if it doesn't
         insertNewTimer(database, worldBoss);
       }
     }) 
   })
 }
-
+/**
+ * Adds new timer Timer table
+ * @param database - yagi database
+ * @param worldBoss - validated world boss data
+ */
 const insertNewTimer = (database, worldBoss) => {
   database.run('INSERT INTO Timer(uuid, last_retrieved_at, location, next_spawn, projected_next_spawn, accurate) VALUES ($uuid, $last_retrieved_at, $location, $next_spawn, $projected_next_spawn, $accurate)', {
     $uuid: generateUUID(),
@@ -30,7 +50,20 @@ const insertNewTimer = (database, worldBoss) => {
     }
   })
 }
-
+/**
+ * Updates data of existing timer with new details in database
+ * To be accurate as possible, we're updating every column on every function call
+ * @param {*} worldBoss - validated world boss data
+ * @param {*} timer - existing timer in table
+ */
+const updateTimerData = (worldBoss, timer) => {
+  let database = new sqlite.Database('./database/yagi.db', sqlite.OPEN_READWRITE);
+  database.run(`UPDATE Timer SET last_retrieved_at = ${Date.now()}, location = "${worldBoss.location}", next_spawn = "${worldBoss.nextSpawn}", projected_next_spawn = "${worldBoss.projectedNextSpawn}", accurate = ${worldBoss.accurate} WHERE uuid = "${timer.uuid}"`, error => {
+    if(error){
+      console.log(error);
+    }
+  })
+}
 const getCurrentTimerData = () => {
   let database = new sqlite.Database('./database/yagi.db', sqlite.OPEN_READWRITE);
   database.get(`SELECT * FROM Timer WHERE rowid = ${1}`, async (error, timer) => {
@@ -45,15 +78,11 @@ const getCurrentTimerData = () => {
       const validatedWorldBossData = validateWorldBossData(worldBossData, serverTime);
       console.log(validatedWorldBossData);
 
-      database.run(`UPDATE Timer SET last_retrieved_at = ${Date.now()}, location = "${validatedWorldBossData.location}", next_spawn = "${validatedWorldBossData.nextSpawn}", projected_next_spawn = "${validatedWorldBossData.projectedNextSpawn}", accurate = ${validatedWorldBossData.accurate} WHERE uuid = "${timer.uuid}"`, error => {
-        if(error){
-          console.log(error);
-        }
-        console.log('Get new data and updated');
-      })
+      updateTimerData(validatedWorldBossData, timer);
     }
   })
 }
+
 module.exports = {
   createTimerTable,
   insertNewTimer,
