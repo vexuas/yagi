3ba973e6a5d4e00b4ba8a46f036f8601f82d04b1
Gabe R <rodriguezgab1601@gmail.com>
2021-12-08 03:30:23 +0800

    Add custom prefix

--- commands/misc/setPrefix.js
+++ commands/misc/setPrefix.js
@@ -2,45 +2,56 @@
 const { defaultPrefix } = require('../../config/yagi.json');
 const { codeBlock } = require('../../helpers');
 
-const generateEmbed = function designOfPrefixEmbed(currentPrefix) {
-  const embed = {
-    color: 32896,
+const generateErrorEmbed = (type, prefix) => {
+  let embed = {
+    color: 16711680,
+    title: 'Whoops!',
+    description: '',
     fields: [
       {
-        name: 'Current Prefix',
-        value: currentPrefix,
-        inline: true,
-      },
-      {
-        name: 'Usage Example',
-        value: `${currentPrefix}help`,
-        inline: true,
-      },
-    ],
-  };
+        name: 'Example',
+        value: '```fix\n\n' + `${prefix}setprefix` +' `newPrefix`\n' + '```'
+      }
+    ]
+  }
+  switch(type){
+    case 'empty':
+     embed.description = 'New prefix cannot be empty!';
+    break;
+    case 'incorrect':
+      embed.description = 'To set a new prefix, make sure to add your new prefix between ``';
+    break;
+    case 'admin': 
+      embed.description = 'Only users with Admin privileges can set a new custom prefix';
+      embed.fields = undefined;
+    break;
+  }
   return embed;
-};
-
+}
 module.exports = {
   name: 'setprefix',
   description: `Sets yagi's prefix to a custom one`,
   hasArguments: true,
   execute(message, arguments, yagi, commands, yagiPrefix) {
-    const currentPrefix = defaultPrefix;
-    
     if(!arguments){
       return message.channel.send('help embed go here');
     }
+    if(!message.member.hasPermission("ADMINISTRATOR")){
+      const embed = generateErrorEmbed('admin', yagiPrefix);
+      return message.channel.send({ embed });
+    }
     if(arguments.startsWith("`") && arguments.endsWith("`") && arguments.length >= 2){
       if(arguments.length === 2){
-        return message.channel.send('New prefix cannot be empty');
+        const embed = generateErrorEmbed('empty', yagiPrefix);
+        return message.channel.send({ embed });
       }
       if(arguments.length > 2){
         const newPrefix = arguments.replace(/\`/g, '');
         return message.channel.send('New prefix successfully set!' + ` ${codeBlock(newPrefix)}`);
       }
     } else {
-      return message.channel.send('To set a new prefix, make sure to add your new prefix between ``')
+      const embed = generateErrorEmbed('incorrect', yagiPrefix);
+      return message.channel.send({ embed })
     }
   }
 };
