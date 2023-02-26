e29d1ea24b616e921d161fb3765391872113322c
Gab <rodriguezgab1601@gmail.com>
2023-02-26 15:29:38 +0800

    Add create guild table handler

--- database/index.js
+++ database/index.js
@@ -2,10 +2,20 @@ const { Pool } = require('pg');
 const { databaseConfig } = require('../config/database');
 const pool = new Pool(databaseConfig);
 
-export function createGuildTable() {
-  const client = pool.connect();
-  try {
-  } catch (error) {
-  } finally {
+export async function createGuildTable(guildsOfYagi) {
+  const client = await pool.connect();
+  if (client) {
+    try {
+      await client.query('BEGIN');
+      const createGuildTableQuery =
+        'CREATE TABLE IF NOT EXISTS Guild(uuid TEXT NOT NULL PRIMARY KEY, name TEXT NOT NULL, member_count INTEGER NOT NULL, owner_id TEXT NOT NULL)';
+      await client.query(createGuildTableQuery);
+    } catch (error) {
+      await client.query('ROLLBACK');
+      console.log(error);
+      //TODO: Add error handling
+    } finally {
+      client.release();
+    }
   }
 }
