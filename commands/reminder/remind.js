d1b0c3fe74e5546fd16046a315a5ad48e16d843b
Gab <rodriguezgab1601@gmail.com>
2023-02-26 13:48:06 +0800

    Replace remind command with embed information

--- commands/reminder/remind.js
+++ commands/reminder/remind.js
@@ -1,28 +1,15 @@
-const { disableReminder, enableReminder, sendReminderInformation } = require('../../database/reminder-db');
-
 module.exports = {
   name: 'remind',
-  description : 'Activates a reminder inside a channel that pings users when world boss is spawning soon',
+  description:
+    'Activates a reminder inside a channel that pings users when world boss is spawning soon',
   hasArguments: true,
-  execute(message, arguments, yagi, commands, yagiPrefix){
-    /**
-     * Accepts 1 argument which can either be 'enable' and 'disable' which fires the relevant functions
-     * If no argument is passed, a reminder instruction function is displayed
-     * To prevent channel spam and abuse, enabling/disabling reminders can only be used by admins
-     * For documentation of functions see reminder-db file
-     */
-    const isAdmin = message.member.hasPermission("ADMINISTRATOR");
-    if(arguments){
-      switch (arguments) {
-        case 'enable':
-          return isAdmin ? enableReminder(message, yagi) : message.channel.send('Reminders can only be enabled by an admin');
-        case 'disable':
-          return isAdmin ? disableReminder(message, yagi) : message.channel.send('Reminders can only be disabled by an admin');
-        default:
-          return message.channel.send('Only accepts `enable` or `disable` as arguments');
-      }
-    } else {
-      sendReminderInformation(message, yagi);
-    }
-  }
-}
+  async execute(message) {
+    const embed = {
+      title: 'Reminder',
+      color: 32896,
+      description:
+        'Temporary disabling the `remind` command for now as a newer and better version of it is currently being developed.\n\nIf you had this feature enabled before, feel free to delete the reminder channel and role. Sorry for the inconvenience!',
+    };
+    await message.channel.send({ embeds: [embed] });
+  },
+};
