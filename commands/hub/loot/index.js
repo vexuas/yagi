fd26e3c5606ab36a8e3c14c9ba18871e28e6e633
Gab <rodriguezgab1601@gmail.com>
2023-03-04 20:51:54 +0800

    Create slash command for loot

--- commands/hub/loot/index.js
+++ commands/hub/loot/index.js
@@ -1,11 +1,28 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
 
 module.exports = {
-  data: new SlashCommandBuilder().setName('loot').setDescription('placeholder'),
+  data: new SlashCommandBuilder()
+    .setName('loot')
+    .setDescription('Displays the loot dropped by World Bosses'),
   async execute({ interaction }) {
     try {
-      await interaction.deferReply();
-      await interaction.editReply('Loot command');
+      const embed = {
+        color: 32896,
+        thumbnail: {
+          url: 'https://cdn.aurakingdom-db.com/file/bb-akdb/images/icons/I00025.png',
+        },
+        footer: {
+          text: 'Opening the chest gives a chance in obtaining any one of these items',
+        },
+        fields: [
+          {
+            name: 'Loot | BattleFront Merit Chest',
+            value:
+              "• Hidden Demon's Hood\n• Baphomet's Assault Armor\n• Demon's Fortified Warhelm\n• Bisolen's Bulwark Armor\n• Healing Potion (Non-Tradable)\n• Secret Stone Randomizer (Non-Tradable)\n• Treasure Charm (Non-Tradable)\n• 50 Loyalty Points (Non-Tradable)\n• Aura Kingdom Coupon: 2000 Points\n• Banoleth Scythe\n• King Banoleth Scythe",
+          },
+        ],
+      };
+      await interaction.reply({ embeds: [embed] });
     } catch (error) {
       console.log(error);
     }
