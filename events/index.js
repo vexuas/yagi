5bafe3d781ba2186036686ff64164944953e250a
Gab <rodriguezgab1601@gmail.com>
2023-03-10 17:07:45 +0800

    Create function to read through files

--- events/index.js
+++ events/index.js
@@ -1,3 +1,5 @@
+const fs = require('fs');
+const path = require('path');
 const { getApplicationCommands } = require('../commands');
 const anything = require('./ready');
 const anythingTwo = require('./guildCreate');
@@ -9,6 +11,24 @@ const anythingSix = require('./interactionCreate');
 const appCommands = getApplicationCommands();
 
 exports.registerEventHandlers = ({ yagi, mixpanel }) => {
+  const loadModules = (directoryPath) => {
+    fs.readdir(directoryPath, { withFileTypes: true }, (error, files) => {
+      if (error) {
+        console.log(error);
+      }
+      files.forEach((file) => {
+        const filePath = path.join(directoryPath, file.name);
+        if (file.isDirectory()) {
+          return loadModules(filePath);
+        }
+        if (file.name === 'index.js') {
+          console.log('wtf');
+        }
+      });
+    });
+  };
+  loadModules('./events');
+
   anything(yagi, appCommands);
   anythingTwo(yagi);
   anythingThree(yagi);
