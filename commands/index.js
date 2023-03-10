d5eb5fff9721e0fef5ea3551040fe323d24146be
Gab <rodriguezgab1601@gmail.com>
2023-03-10 22:02:40 +0800

    Create load modules handler

--- commands/index.js
+++ commands/index.js
@@ -1,7 +1,30 @@
-//TODO: Add a loop to import all of these instead of manually importing each one
+const fs = require('fs');
+const path = require('path');
+
 exports.getApplicationCommands = () => {
+  const appCommands = {};
+  const loadModules = (directoryPath) => {
+    fs.readdir(directoryPath, { withFileTypes: true }, (error, files) => {
+      if (error) {
+        console.log(error);
+      }
+      files.forEach((file) => {
+        const filePath = path.join(directoryPath, file.name);
+        if (file.isDirectory()) {
+          return loadModules(filePath);
+        }
+        if (file.name === 'index.js') {
+          const modulePath = `./${filePath.replace('commands/', '')}`;
+          appCommands[directoryPath.replace('commands/', '')] = require(modulePath);
+          console.log(directoryPath.replace('commands/', ''));
+        }
+      });
+    });
+  };
+  loadModules('./commands');
+  console.log(appCommands);
   return {
-    goats: require('./goats/'),
+    goats: require('./goats'),
     remind: require('./remind'),
     help: require('./help'),
     about: require('./about'),
