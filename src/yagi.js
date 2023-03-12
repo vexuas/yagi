1ec92d600fa37016c3ebffcf973bddcf80daebf3
Gab <rodriguezgab1601@gmail.com>
2023-03-12 13:28:19 +0800

    Replace config variables on initialise

--- src/yagi.js
+++ src/yagi.js
@@ -1,25 +1,21 @@
 const Discord = require('discord.js');
-const { token, bisoMixpanel, yagiMixpanel, topggToken } = require('./config/yagi.json');
 const yagi = new Discord.Client({
   intents: [Discord.Intents.FLAGS.GUILDS, Discord.Intents.FLAGS.GUILD_MESSAGES],
 });
 const Mixpanel = require('mixpanel');
 const { AutoPoster } = require('topgg-autoposter');
 const { registerEventHandlers } = require('./events/events.js');
-const { checkIfInDevelopment } = require('./utils/helpers');
-const { defaultPrefix } = require('./config/environment');
-let mixpanel;
+const { BOT_TOKEN, MIXPANEL_ID, TOP_GG_TOKEN } = require('./config/environment');
 
-console.log(defaultPrefix);
 //----------
 /**
  * Initialize yagi to log in and establish a connection to Discord
  * Wrapped in an async function as we want to wait for the promise to end so that our mixpanel instance knows which project to initialize in
  */
 const initialize = async () => {
-  await yagi.login(token);
-  mixpanel = Mixpanel.init(checkIfInDevelopment(yagi) ? bisoMixpanel : yagiMixpanel);
-  !checkIfInDevelopment(yagi) && AutoPoster(topggToken, yagi);
+  await yagi.login(BOT_TOKEN);
+  const mixpanel = Mixpanel.init(MIXPANEL_ID);
+  TOP_GG_TOKEN.length !== 0 && AutoPoster(TOP_GG_TOKEN, yagi);
   registerEventHandlers({ yagi, mixpanel });
 };
 initialize();
