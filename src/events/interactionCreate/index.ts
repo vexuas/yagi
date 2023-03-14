1fffad9f3f95c8d44603fccf0135f8e1c49fdce0
Gab <rodriguezgab1601@gmail.com>
2023-03-14 22:35:25 +0800

    Convert analytics and interactionCreate

--- src/events/interactionCreate/index.ts
+++ src/events/interactionCreate/index.ts
@@ -1,13 +1,22 @@
-const { sendMixpanelEvent } = require('../../services/analytics');
+import { CacheType, Client, Interaction } from 'discord.js';
+import { Mixpanel } from 'mixpanel';
+import { sendMixpanelEvent } from '../../services/analytics';
 
-module.exports = ({ yagi, appCommands, mixpanel }) => {
-  yagi.on('interactionCreate', async (interaction) => {
+interface Props {
+  yagi: Client;
+  appCommands: any;
+  mixpanel: Mixpanel;
+}
+export default function ({ yagi, appCommands, mixpanel }: Props) {
+  yagi.on('interactionCreate', async (interaction: Interaction<CacheType>) => {
     if (!interaction.inGuild()) return; //Only respond in server channels or if it's an actual command
 
     if (interaction.isCommand()) {
       const { commandName } = interaction;
       await appCommands[commandName].execute({ interaction, yagi });
       mixpanel &&
+        interaction.channel &&
+        interaction.guild &&
         sendMixpanelEvent({
           user: interaction.user,
           channel: interaction.channel,
@@ -17,4 +26,4 @@ module.exports = ({ yagi, appCommands, mixpanel }) => {
         }); //Send tracking event to mixpanel
     }
   });
-};
+}
