bd7712f79968baf0c640056c05fdc198b3777563
Gab <rodriguezgab1601@gmail.com>
2023-03-20 23:51:56 +0800

    Add tests for sheets command

---
+++ src/commands/sheets/index.test.ts
@@ -0,0 +1,18 @@
+import { generateSheetsEmbed } from '.';
+
+describe('Invite Command', () => {
+  it('generates an embed correctly', () => {
+    const embed = generateSheetsEmbed();
+
+    expect(embed).not.toBeUndefined();
+  });
+  it('displays the correct fields in the embed', () => {
+    const embed = generateSheetsEmbed();
+
+    expect(embed.title).not.toBeUndefined();
+    expect(embed.color).not.toBeUndefined();
+    expect(embed.description).not.toBeUndefined();
+    expect(embed.fields).not.toBeUndefined();
+    expect(embed.fields && embed.fields.length).toBe(1);
+  });
+});
--- src/commands/sheets/index.ts
+++ src/commands/sheets/index.ts
@@ -1,5 +1,24 @@
 import { AppCommand, AppCommandOptions } from '../commands';
 import { SlashCommandBuilder } from '@discordjs/builders';
+import { APIEmbed } from 'discord-api-types/v9';
+
+export const generateSheetsEmbed = (): APIEmbed => {
+  const embed = {
+    title: 'Public Sheets',
+    description:
+      'Kudos to all the leads and editors for being awesome and making all this possible!\nFor more information about the Olympus World Boss, do visit these links!\n[Olympus Sheet](https://docs.google.com/spreadsheets/d/tUL0-Nn3Jx7e6uX3k4_yifQ/edit#gid=585652389) | [Olympus Discord](https://discordapp.com/invite/bC9zWYs) | [Olympus Recruitment](https://docs.google.com/forms/d/e/1FAIpQLSfcLEBugvL70lLCttUiSHw_Cc6wI8S1te6X-CAsP8N-dr6u_w/viewform)',
+    color: 32896,
+    fields: [
+      {
+        name: 'OG links',
+        value:
+          '[Catalyst (Chimera)](https://tinyurl.com/catalyst-ak)\n[Angels (Phoenix)](https://docs.google.com/spreadsheets/d/1MrMwNerILxNK0lvKCBklkYZx_OKAb4io8XdaldRyO_g/edit#gid=1305398803)\n[Lazy Goat (Phoenix)](https://tinyurl.com/LazyGoatWB)',
+        inline: true,
+      },
+    ],
+  };
+  return embed;
+};
 
 export default {
   data: new SlashCommandBuilder()
@@ -7,20 +26,7 @@ export default {
     .setDescription('Collection of current and previous sheets used by leads/editors'),
   async execute({ interaction }: AppCommandOptions) {
     try {
-      const embed = {
-        title: 'Public Sheets',
-        description:
-          'Kudos to all the leads and editors for being awesome and making all this possible!\nFor more information about the Olympus World Boss, do visit these links!\n[Olympus Sheet](https://docs.google.com/spreadsheets/d/tUL0-Nn3Jx7e6uX3k4_yifQ/edit#gid=585652389) | [Olympus Discord](https://discordapp.com/invite/bC9zWYs) | [Olympus Recruitment](https://docs.google.com/forms/d/e/1FAIpQLSfcLEBugvL70lLCttUiSHw_Cc6wI8S1te6X-CAsP8N-dr6u_w/viewform)',
-        color: 32896,
-        fields: [
-          {
-            name: 'OG links',
-            value:
-              '[Catalyst (Chimera)](https://tinyurl.com/catalyst-ak)\n[Angels (Phoenix)](https://docs.google.com/spreadsheets/d/1MrMwNerILxNK0lvKCBklkYZx_OKAb4io8XdaldRyO_g/edit#gid=1305398803)\n[Lazy Goat (Phoenix)](https://tinyurl.com/LazyGoatWB)',
-            inline: true,
-          },
-        ],
-      };
+      const embed = generateSheetsEmbed();
       await interaction.reply({ embeds: [embed] });
     } catch (error) {
       console.log(error);
