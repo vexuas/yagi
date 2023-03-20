d1b741232a3e4c50a6cfe6b6ee0910334db4111d
Gab <rodriguezgab1601@gmail.com>
2023-03-20 21:49:30 +0800

    Add test for about command

--- src/commands/about/index.test.ts
+++ src/commands/about/index.test.ts
@@ -1,1 +1,34 @@
-it.only('this is a test', () => {});
+import { Client } from 'discord.js';
+import { generateAboutEmbed } from '.';
+import { BOT_VERSION } from '../../version';
+
+describe('About Command', () => {
+  it('generates an embed correctly', () => {
+    const embed = generateAboutEmbed();
+
+    expect(embed).not.toBeUndefined();
+  });
+  it('displays the correct fields in the embed', () => {
+    const embed = generateAboutEmbed();
+
+    expect(embed.title).toBeDefined();
+    expect(embed.description).toBeDefined();
+    expect(embed.color).toBeDefined();
+    expect(embed.thumbnail.url).toBeDefined();
+    expect(embed.fields).toBeDefined();
+    expect(embed.fields.length).toBe(6);
+  });
+  it('displays the correct description if client is passed in', () => {
+    const yagi = new Client({ intents: [] });
+    const embed = generateAboutEmbed(yagi);
+
+    expect(embed).not.toBeUndefined();
+    expect(embed.description).toBeDefined();
+    expect(embed.description.includes('0 servers')).toBeTruthy();
+  });
+  it('displays the correct bot version', () => {
+    const embed = generateAboutEmbed();
+    expect(embed.fields[2].name).toBe('Version');
+    expect(embed.fields[2].value).toBe(BOT_VERSION);
+  });
+});
